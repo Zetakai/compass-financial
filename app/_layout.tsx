e6718fbc086ff80f1381e9c0d660ca9f027dc326
Zaki <muhammadfarid.zaki@gmail.com>
2025-12-16 23:18:01 +0700

    chore: Add react-native-gesture-handler import to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
-import 'react-native-gesture-handler';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+  ErrorBoundary
 } from 'expo-router';
 
 export const unstable_settings = {
